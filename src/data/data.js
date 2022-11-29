fb92e80336e2ca6bc8d42b661e3ff9ee61220c7d
marios <mgeorgak859@gmail.com>
2022-11-29 18:19:23 +0200

    updates

--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,29 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
+import FoodTime from '../assets/projects/foodtime.jpg'
+import Ecommerce from '../assets/projects/ecommerce.jpg'
+import Monsters from '../assets/projects/monster.png'
 
 export const data=[
     {
         id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        name:"Food Time Application",
+        image:FoodTime,
+        github:"https://github.com/satwswy/FoodTimeApp-fe",
+        live:"https://foodtime.onrender.com/",
     },
     {
         id:2,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        name:"E-Commerce Application",
+        image:Ecommerce,
+        github:"https://github.com/satwswy/e-clothing",
+        live:"https://whimsical-sunburst-207b23.netlify.app/",
     },
     {
         id:3,
         name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
-    {
-        id:4,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
-    },
+        image:Monsters,
+        github:"https://github.com/satwswy/Rolodex-Monsters",
+        live:"https://rolodex-monsters.onrender.com/",
+    }
 
 
 ]
