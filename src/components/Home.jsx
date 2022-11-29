bbebeaefeaacc1822285fc596933761b7ff4ce7a
marios <mgeorgak859@gmail.com>
2022-11-29 10:43:54 +0200

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,14 +8,13 @@ const Home = () => {
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
-          SAI KUMAR
+          Marios Georgakopoulos
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
           I'm a Full Stack Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
+          I’m a full-stack developer specializing in building exceptional digital experiences. Currently, I’m focused on
           building responsive full-stack web applications.
         </p>
         <div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,8 +7,9 @@ import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
 import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
-import Tailwind from '../assets/tailwind.png';
 import Mongo from '../assets/mongo.png';
+import Ts from '../assets/ts.png';
+import Redux from '../assets/redux.png';
 
 const Skills = () => {
   return (
@@ -50,12 +51,16 @@ const Skills = () => {
                   <p className='my-4'>MONGO DB</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
-                  <p className='my-4'>TAILWIND</p>
+                  <img className='w-20 mx-auto' src={Ts} alt="HTML icon" />
+                  <p className='my-4'>TYPESCRIPT</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
                   <p className='my-4'>FIREBASE</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Redux} alt="HTML icon" />
+                  <p className='my-4'>REDUX</p>
               </div>
           </div>
       </div>
