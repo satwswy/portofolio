5d9500bf5f317227a55fa2d76e2b3476593550ed
marios <mgeorgak859@gmail.com>
2022-11-29 19:17:31 +0200

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import { Link } from 'react-scroll';
 import Profile from '../assets/projects/profile.jpg'
+import "./Home.css"
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <div className="hey" style={{backgroundImage: `url(${Profile})`}}></div>
         <p className='text-pink-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
           Marios Georgakopoulos
